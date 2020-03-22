6c7b43958227ac7feb82f535f565b06b63382888
wangliang <wg_lg520@21cn.com>
2020-03-22 20:17:45 +0800

    commit by wangliang

--- src/main/java/com/rabbit/demo/config/RabbitConfig.java
+++ src/main/java/com/rabbit/demo/config/RabbitConfig.java
@@ -6,6 +6,17 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * @Description: 在rabbitmq中，exchange有4个类型：direct，topic，fanout，header。
+ * Direct
+ * 直接交换器，工作方式类似于单播，Exchange会将消息发送完全匹配ROUTING_KEY的Queue
+ *
+ * fanout
+ * 广播是式交换器，不管消息的ROUTING_KEY设置为什么，Exchange都会将消息转发给所有绑定的Queue。
+ *
+ * topic
+ * 主题交换器，工作方式类似于组播，Exchange会将消息转发和ROUTING_KEY匹配模式相同的所有队列，比如，ROUTING_KEY为user.stock的Message会转发给绑定匹配模式为 * .stock,user.stock， * . * 和#.user.stock.#的队列。（ * 表是匹配一个任意词组，#表示匹配0个或多个词组）
+ *
+ * headers
+ * 消息体的header匹配（ignore）
  * @Param:
  * @return:
  * @Author: wangliang
